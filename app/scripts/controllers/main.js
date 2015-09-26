94debe809dabf1a97643e96f7a3f3c2cbce81d5f
Jason Yeomans <jyeo098@gmail.com>
2015-09-26 18:56:46 -0400

    main page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,15 @@
  * Controller of the movieHackGtFrontendApp
  */
 angular.module('movieHackGtFrontendApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function($http) {
+    // $http.post('/someUrl', {
+    //   msg: 'hello word!'
+    // }).
+    // then(function(response) {
+    //   // this callback will be called asynchronously
+    //   // when the response is available
+    // }, function(response) {
+    //   // called asynchronously if an error occurs
+    //   // or server returns response with an error status.
+    // });
   });
