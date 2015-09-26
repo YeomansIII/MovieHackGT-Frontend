e1071436e49c7facccdaa8e51cbe4cb39994ede2
Jason Yeomans <jyeo098@gmail.com>
2015-09-25 23:12:23 -0400

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name movieHackGtFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the movieHackGtFrontendApp
+ */
+angular.module('movieHackGtFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
